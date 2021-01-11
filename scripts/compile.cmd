54e19b8381034e63a20b6be587da45210018526c
Tom Horrocks <tom.horrocks@uwa.edu.au>
2021-01-11 15:11:40 +0800

    Add comment showing how to retrive site_packages

--- scripts/compile.cmd
+++ scripts/compile.cmd
@@ -4,6 +4,8 @@ REM Run this script from the project root, and only when the conda environment i
 REM Compiles the program (assuming entry point is main.py) to "dist\\<project_name>"
 
 set /p project_name=<.\\project_name.txt
+REM site_packages for activated conda environment
+REM set site_packages=%CONDA_PREFIX%\Lib\site-packages
 
 REM Compile into "build" folder
 if exist build rmdir build /s /q
